d9ba962677e54889e9128c5269d1f032cddc3028
noahturnquist <62677377+noahturnquist@users.noreply.github.com>
2021-02-17 12:01:38 -0800

    Create init.py

---
+++ init.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------
+#	init.py
+#	Version: 1.0.1
+#	Last Updated: January 29th 2021
+#-------------------------------------------------------------------
+#
+#-------------------------------------------------------------------
+
+
+import nuke
+
+nuke.pluginAddPath('./icons')
+
+nuke.pluginAddPath('./python')
+nuke.pluginAddPath('./python/noah_lbs')
+
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./gizmos/spin_vfx')
+nuke.pluginAddPath('./gizmos/nukepedia')
